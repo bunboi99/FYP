b75fddc10d8b81ed3f67706ae54d726ff6522d11
wangding <wding139@gmail.com>
2024-01-23 13:50:51 +0800

    added mobilenetSSD obj_detection

--- app.py
+++ app.py
@@ -24,9 +24,12 @@ def index():
 def gen(camera):
     """Video streaming generator function."""
     yield b'--frame\r\n'
-    while True:
-        frame = camera.get_frame()
-        yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
+    try:
+        while True:
+            frame = camera.get_frame()
+            yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
+    except KeyboardInterrupt:
+        camera.release()
 
 
 @app.route('/video_feed')
